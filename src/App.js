62c9ea931f170d236c5f657121f01aaf52ded2f8
klauslube <klauslube93@gmail.com>
2022-06-20 19:01:37 -0300

    fix:rotas

--- src/App.js
+++ src/App.js
@@ -12,7 +12,21 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Login } />
-
+          {/* <Route exact path='/foods' component={}/>
+        <Route exact path='/drinks' component={}/>
+        <Route exact path='/foods/{id-da-receita}' component={}/>
+        <Route exact path='/drinks/{id-da-receita}' component={}/>
+        <Route exact path='/foods/{id-da-receita}/in-progress' component={}/>
+        <Route exact path='/drinks/{id-da-receita}/in-progress' component={}/>
+        <Route exact path='/explore' component={}/>
+        <Route exact path='/explore/foods' component={}/>
+        <Route exact path='/explore/drinks' component={}/>
+        <Route exact path='/explore/foods/ingredients' component={}/>
+        <Route exact path='/explore/drinks/ingredients' component={}/>
+        <Route exact path='/explore/foods/nationalities' component={}/>
+        <Route exact path='/profile' component={}/>
+        <Route exact path='/done-recipes' component={}/>
+        <Route exact path='/favorite-recipes' component={}/> */}
         </Switch>
       </BrowserRouter>
 
