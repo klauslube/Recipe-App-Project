e994421a287d769855ffc0a47ddcfccfd9739014
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-06-20 18:24:31 -0300

    Dev: login page
    Reafac with team members to easy understood.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,90 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { actionCreators } from '../redux/actions';
 
-class Login extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      emailInput: '',
-      passwordInput: '',
-      isButtonDisabled: true,
-      redirect: false,
-    };
-  }
+export default function Login(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  handleChange = ({ target: { value, name } }) => {
-    this.setState({ [name]: value },
-      () => { this.handleError(); });
-  }
+  const handleSubmit = () => {
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+    const { history } = props;
+    history.push('/foods');
+  };
 
-  handleError = () => {
-    const { emailInput, passwordInput } = this.state;
-
-    const minPasswordLength = 6;
-    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-
-    this.setState({
-      isButtonDisabled: !(
-        passwordInput.length >= minPasswordLength && emailRegex.test(emailInput)),
-    });
-  }
-
-  handleSubmitButton = (email) => {
-    const { loginButton } = this.props;
-    loginButton(email);
-    this.setState({ redirect: true });
-  }
-
-  render() {
-    const {
-      emailInput,
-      passwordInput,
-      isButtonDisabled,
-      redirect,
-    } = this.state;
-    return (
-      <section>
-        <form>
-          <input
-            type="email"
-            name="emailInput"
-            data-testid="email-input"
-            placeholder="E-mail"
-            value={ emailInput }
-            onChange={ this.handleChange }
-          />
-          <input
-            type="password"
-            name="passwordInput"
-            data-testid="password-input"
-            placeholder="Senha"
-            value={ passwordInput }
-            onChange={ this.handleChange }
-          />
-          <button
-            type="button"
-            data-testid="login-submit-btn"
-            disabled={ isButtonDisabled }
-            onClick={ () => this.handleSubmitButton(emailInput) }
-          >
-            Entrar
-          </button>
-        </form>
-        { redirect && <Redirect to="/explore" /> }
-      </section>
-    );
-  }
+  const regex = /\S+@\S+\.\S+/;
+  const minLengthPass = 6;
+  return (
+    <form>
+      <label htmlFor="login_email">
+        Email
+        <input
+          data-testid="email-input"
+          type="email"
+          name="email"
+          value={ email }
+          onChange={ ({ target: { value } }) => setEmail(value) }
+          id="login_email"
+        />
+      </label>
+      <label htmlFor="login_password">
+        Email
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          value={ password }
+          onChange={ ({ target: { value } }) => setPassword(value) }
+          id="login_password"
+        />
+      </label>
+      <button
+        type="submit"
+        onClick={ handleSubmit }
+        disabled={ !(password.length > minLengthPass && regex.test(email)) }
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </form>
+  );
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  loginButton: (payload) => dispatch(actionCreators.loginAction(payload)),
-});
-
-export default connect(null, mapDispatchToProps)(Login);
-
 Login.propTypes = {
-  loginButton: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
