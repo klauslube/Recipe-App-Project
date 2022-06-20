934f2021c70d395603bc7de7cb32145f4b339801
klauslube <klauslube93@gmail.com>
2022-06-20 17:43:34 -0300

    Criacao de rotas e componente Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Switch } from 'react-router-dom';
+import {Login} from './components/Login'
 function App() {
   return (
     <Provider store={ store }>
-
+      <Switch>
+        <Route exact path='/' component={ Login }/>
+        <Route exact path='/foods' component={}/>
+        <Route exact path='/drinks' component={}/>
+        <Route exact path='/foods/{id-da-receita}' component={}/>
+        <Route exact path='/drinks/{id-da-receita}' component={}/>
+        <Route exact path='/foods/{id-da-receita}/in-progress' component={}/>
+        <Route exact path='/drinks/{id-da-receita}/in-progress' component={}/>
+        <Route exact path='/explore' component={}/>
+        <Route exact path='/explore/foods' component={}/>
+        <Route exact path='/explore/drinks' component={}/>
+        <Route exact path='/explore/foods/ingredients' component={}/>
+        <Route exact path='/explore/drinks/ingredients' component={}/>
+        <Route exact path='/explore/foods/nationalities' component={}/>
+        <Route exact path='/profile' component={}/>
+        <Route exact path='/done-recipes' component={}/>
+        <Route exact path='/favorite-recipes' component={}/>
+      </Switch>
     </Provider>
   );
 }
