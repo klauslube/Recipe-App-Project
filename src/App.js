eac345c81a571a03dddb49275b474a09c48c4fd2
Frederico Leles Santos <fred.leles@hotmail.com>
2022-06-20 17:49:33 -0300

    SearchBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <Provider store={ store }>
-
-    </Provider>
+    // <Provider store={ store }>
+    <SearchBar />
+    // </Provider>
   );
 }
 
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+
+function SearchBar() {
+  const [searchText, setSearch] = useState('');
+  const [radioSelected, setRadio] = useState();
+
+  const changeSearch = ({ target }) => {
+    setSearch(target.value);
+  };
+
+  const submitSearch = (e) => {
+    e.preventDefault();
+    if (radioSelected === 2 && searchText.length === 1) {
+      alert('Your search must have only 1 (one) character');
+      console.log('ok');
+    }
+    console.log(searchText, radioSelected);
+  };
+
+  return (
+    <section>
+      <form onSubmit={ submitSearch }>
+        <div>
+          <input
+            type="text"
+            data-testid="search-input"
+            value={ searchText }
+            onChange={ changeSearch }
+          />
+        </div>
+        <div>
+          <label htmlFor="searchIngredient">
+            <input
+              id="searchIngredient"
+              type="radio"
+              data-testid="ingredient-search-radio"
+              name="searchInput"
+              onClick={ () => setRadio(0) }
+            />
+            Ingredient
+          </label>
+          <label htmlFor="searchName">
+            <input
+              id="searchName"
+              type="radio"
+              data-testid="name-search-radio"
+              name="searchInput"
+              onClick={ () => setRadio(1) }
+            />
+            Name
+          </label>
+          <label htmlFor="searchLetter">
+            <input
+              id="searchLetter"
+              type="radio"
+              data-testid="first-letter-search-radio"
+              name="searchInput"
+              onClick={ () => setRadio(2) }
+            />
+            First letter
+          </label>
+        </div>
+        <button
+          type="submit"
+          data-testid="exec-search-btn"
+        >
+          Search
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default SearchBar;
