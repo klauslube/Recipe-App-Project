750fc06275ed048d01d513558957d936fca6576a
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-06-21 16:23:54 -0300

    Dev:update

--- src/helpers/renderWithRouterAndRedux.js
+++ src/helpers/renderWithRouterAndRedux.js
@@ -3,9 +3,9 @@ import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
-import {  legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import reducer from '../redux/reducers';
+import reducer from '../reducers';
 
 export const renderWithRouterAndRedux = (component, initialState, route = '/') => {
   const store = createStore(reducer, initialState, applyMiddleware(thunk));
