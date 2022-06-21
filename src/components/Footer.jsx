40856c46f373fdd8b05bfa2b71667a3805a36878
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-06-21 15:17:38 -0300

    Dev: updated pages food with component footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import PropTypes from 'prop-types';
+// import { useHistory } from 'react-router-dom';
+// import PropTypes from 'prop-types';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import foodIcon from '../images/mealIcon.svg';
 
-function Footer() {
+export default function Footer() {
   return (
     <div atributo data-testid="footer" id="footer">
       <input
-        onClick={ PushProfile }
         type="image"
         data-testid="drinks-bottom-btn"
         src={ drinkIcon }
         alt="drinks-bottom-btn"
       />
       <input
-        onClick={ PushProfile }
         type="image"
         data-testid="explore-bottom-btn"
         src={ exploreIcon }
         alt="explore-bottom-btn"
       />
       <input
-        onClick={ PushProfile }
         type="image"
         data-testid="food-bottom-btn"
         src={ foodIcon }
@@ -32,5 +29,3 @@ function Footer() {
     </div>
   );
 }
-
-export default Footer;
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Foods() {
   const history = useHistory();
   return (
-    <Header history={ history } />
+    <>
+      <Header history={ history } />
+      <Footer />
+    </>
+
   );
 }
