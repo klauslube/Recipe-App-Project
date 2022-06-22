46edc6c31e659ac56db7caecfbe638abaa7e23eb
Frederico Leles Santos <fred.leles@hotmail.com>
2022-06-22 15:05:33 -0300

    RecipesList - commit inicial

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RecipeCards from '../components/RecipeCards';
+import fetchApi from '../helpers/fetchApi';
+import { actionCreators } from '../redux/actions';
 
 export default function Drinks() {
+  const { setDrinksAction } = actionCreators;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    fetchApi('/drinks', 1, '').then((res) => {
+      dispatch(setDrinksAction(res.drinks));
+    });
+  }, []);
   return (
     <div>
       <Header title="Drinks" search />
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RecipeCards from '../components/RecipeCards';
+import fetchApi from '../helpers/fetchApi';
+import { actionCreators } from '../redux/actions';
 
 export default function Foods() {
+  const { setMealsAction } = actionCreators;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    fetchApi('/foods', 1, '').then((res) => {
+      dispatch(setMealsAction(res.meals));
+    });
+  }, []);
+
   return (
     <>
       <Header title="Foods" search />
