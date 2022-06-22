5828b7a0870278dbe726090cfb510b2489bf71c6
klauslube <klauslube93@gmail.com>
2022-06-22 14:53:44 -0300

    Dev:adicionado form

--- src/pages/DrinksDetails.jsx
+++ src/pages/DrinksDetails.jsx
@@ -3,7 +3,34 @@ import React from 'react';
 export default function DrinksDetails() {
   return (
     <div>
-      <h1>Drinks Details</h1>
+      <form>
+        <img data-testid="recipe-photo" src="" alt="" />
+        <p data-testid="recipe-title">titulo receita</p>
+        <button type="button" data-testid="share-btn">compartilhar</button>
+        <button type="button" data-testid="favorite-btn">favoritar</button>
+        <p data-testid="recipe-category">categoria</p>
+
+        <div>
+          <p>Ingredients</p>
+          <div data-testid="${index}-ingredient-name-and-measure">
+
+          </div>
+        </div>
+
+        <div>
+          <p>Instructions</p>
+          <div data-testid="instructions" >
+
+          </div>
+        </div>
+        <div>
+          <p>Recommended</p>
+          <div data-testid="${index}-recomendation-card" >
+
+          </div>
+        </div>
+        <button type='submit' data-testid="start-recipe-btn"></button>
+      </form>
     </div>
   );
 }
